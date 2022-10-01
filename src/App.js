4e0cb7c4f1b8459b229b9fb6852a58a99dd2a591
Jirapat <bukharney@gmail.com>
2022-10-01 23:55:23 +0700

    1-5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Transaction from "./components/Transaction";
 import FormComponent from "./components/Form";
 
 import './App.css';
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import DataContext from "./data/DataContext";
 import ReportComponent from "./components/ReportComponent";
 import { element } from "prop-types";
@@ -10,10 +10,10 @@ import { element } from "prop-types";
 function App() {
   const design = {color: 'red',textAlign: 'center',fontSize: '1.5rem'}
   const [items , setItems] = useState([
-    {id: 0, title: "fesfds", amount: 5000},
-    {id: 1, title: "fesfds", amount: -1000},
-    {id: 2, title: "fesfds", amount: -2000},
-    {id: 3, title: "fesfds", amount: 3000},
+    {id: 0, title: "income", amount: 5000},
+    {id: 1, title: "expense", amount: -1000},
+    {id: 2, title: "expense", amount: -2000},
+    {id: 3, title: "income", amount: 3000},
   ]);
   const [reportIncome, setReportIncome] = useState(0)
   const [reportExpense, setReportExpense] = useState(0)
@@ -28,7 +28,18 @@ function App() {
       const expense = amounts.filter(element=>element<0).reduce((total,element)=>total+=element,0)
       setReportExpense(expense)
       setReportIncome(income)
-    },[items,reportIncome,reportExpense])
+  },[items,reportIncome,reportExpense])
+
+  const [showReport, setshowReport] = useState(false)
+  const reducer = (state, action)=>{
+    switch(action.type){
+      case "SHOW" :
+        return setshowReport(true)
+      case "HIDE" : 
+        return setshowReport(false)
+    }
+  }
+  const [result,dispatch] = useReducer(reducer,showReport)
   return (
     <DataContext.Provider value={{
       income:  reportIncome,
@@ -36,9 +47,11 @@ function App() {
     }}>
       <div className='container'>
         <h1 style={design}>Income - Expenses Program</h1>
-        <ReportComponent/>
+        {showReport && <ReportComponent/>}
         <FormComponent onAddItem = {onAddNameItem}/>
         <Transaction items = {items}/>
+        <button onClick={()=>{dispatch({type:"SHOW"})}}>SHOW</button>
+        <button onClick={()=>{dispatch({type:"HIDE"})}}>HIDE</button>
     </div>
     </DataContext.Provider>
   );
